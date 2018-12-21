aff79b2c0daceacda0ed6751c8acf594f6be8bde
narendra123dasari <46023137+narendra123dasari@users.noreply.github.com>
2018-12-21 17:05:09 +0530

    Add files via upload

---
+++ narendra/addition.c
@@ -0,0 +1,13 @@
+//sum of two numbers
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+   int a,b,c;
+   clrscr(); 
+   printf("enter a&b values\n");
+   scanf("%d%d",&a,&b); 
+  c=a+b;
+  printf("sum of two numbers is:%d",c);
+getch();
+}
